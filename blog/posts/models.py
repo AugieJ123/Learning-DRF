6148c5ced0fac721fd421b529f475a36a195667f
augiej123 <jibbanani@gmail.com>
2022-07-02 06:02:29 +0000

    version 1 of the blog api

---
+++ blog/posts/models.py
@@ -0,0 +1,13 @@
+from turtle import update
+from django.db import models
+from django.conf import settings
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title

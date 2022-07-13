abde317d4209eaee167129661f70df1a0f039a8c
augiej123 <jibbanani@gmail.com>
2022-07-13 04:17:35 +0000

    makig views with generics

--- blog/posts/serializers.py
+++ blog/posts/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from .models import Post
@@ -6,3 +7,8 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ('id', 'author', 'title', 'body', 'created_at',)
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username', 'email')
--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import PostList, PostDetail
+from .views import PostList, PostDetail, UserList, UserDetail
 
 urlpatterns = [
     path('<int:pk>/', PostDetail.as_view(), name='post_detail'),
     path('', PostList.as_view(), name='post_list'),
+    path('users/', UserList.as_view(), name='user_list'),
+    path('users/<int:pk>/', UserDetail.as_view(), name='user_detail'),
 ]
